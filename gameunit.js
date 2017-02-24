ef5eb4b31006472f84939e8f52cc44c43eb56af1
WangShuai <3087725379@qq.com>
2017-02-24 23:42:10 +0800

    有效的预测补偿同步算法

--- gameunit.js
+++ gameunit.js
@@ -2,8 +2,13 @@ function GameUnit(x, y, speed, scene, client) {
     this.client = client;
     this.speed = speed;
     this.syncState = {pos: new THREE.Vector3(x, y, 0)};
+    this.simuState = {pos: new THREE.Vector3(x, y, 0)};
     this.showState = {pos: new THREE.Vector3(x, y, 0)};
     this.target = new THREE.Vector3(x, y, 0);
+
+    this.showCpPos = new THREE.Vector3();
+    this.showCpStart = 0;
+    this.showCpLast = 0;
 
     this.netFrameIndex = 0;
     this.syncNetFrameIndex = 0;
@@ -13,14 +18,6 @@ function GameUnit(x, y, speed, scene, client) {
     //sprite.add(head);
     this.sprite = sprite;
     scene.add(this.sprite);
-
-    /*
-    GameUnit.prototype.updateShowState = function () {
-        this.showState.pos.lerp(this.realState.pos, 0.9);
-        this.sprite.position.x = this.realState.pos.x;
-        this.sprite.position.y = this.realState.pos.y;
-    };
-    */
 
     GameUnit.prototype.handleNetFrame = function (netFrameIndex) {
         this.netFrameIndex = netFrameIndex;
@@ -32,27 +29,42 @@ function GameUnit(x, y, speed, scene, client) {
         this.syncState.pos = util.move(this.syncState.pos,
                 this.target, this.speed, deltaTime);
         this.target = msg.target.clone();
-        console.log(this.showState.pos);
-        this.showState.pos = this.syncState.pos.clone();
-        console.log(this.showState.pos);
+
+        //this.showState.pos = this.syncState.pos.clone();
+        deltaTime = (UpdateHandles.time - this.client.startTime) 
+            - msg.frameIndex * config.netFrameInterval;
+        this.simuState.pos = util.move(this.syncState.pos,
+            this.target, this.speed, deltaTime);
+        //2帧内补偿showDelta
+        this.showCpPos.copy(this.simuState.pos).sub(this.showState.pos);
+        this.showCpStart = UpdateHandles.time;
+        this.showCpLast = this.showCpStart;
+
         this.syncNetFrameIndex = msg.frameIndex;
     };
 
-    GameUnit.prototype.simulateShowState = function () {
-        if (this.netFrameIndex <= this.syncNetFrameIndex) {
-            return;
-        }
-        var dis = this.target.distanceTo(this.showState.pos);
+    GameUnit.prototype.simulate = function () {
+        var oldSimuPos = this.simuState.pos.clone();
+        var dis = this.target.distanceTo(this.simuState.pos);
         if (dis >= config.targetDelta) {
             var deltaTime = (UpdateHandles.time - this.client.startTime) 
                 - this.syncNetFrameIndex * config.netFrameInterval;
-            this.showState.pos = util.move(this.syncState.pos,
+            this.simuState.pos = util.move(this.syncState.pos,
                 this.target, this.speed, deltaTime);
         }
+        return this.simuState.pos.clone().sub(oldSimuPos);
     };
 
     GameUnit.prototype.update = function () {
-        this.simulateShowState();
+        var translate = this.simulate();
+        this.showState.pos.add(translate);
+        var cpHead = Math.min(UpdateHandles.time, 
+            this.showCpStart + 2 * config.netFrameInterval);
+        var cpAlpha = (cpHead - this.showCpLast)
+            / (2 * config.netFrameInterval);
+        var cpPos = this.showCpPos.clone().multiplyScalar(cpAlpha);
+        this.showState.pos.add(cpPos);
+        this.showCpLast = cpHead;
         this.sprite.position.x = this.showState.pos.x;
         this.sprite.position.y = this.showState.pos.y;
         /*
