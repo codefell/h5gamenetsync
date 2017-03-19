92d0b346350d84933bed9409681af51b87666ac4
WangShuai <3087725379@qq.com>
2017-03-19 21:39:27 +0800

    no comment

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 $(function () {
+    var wsServer = 'ws://localhost:8000';
+    window.websocket = new WebSocket(wsServer); 
+    websocket.onopen = function (evt) { onOpen(evt) }; 
+    websocket.onclose = function (evt) { onClose(evt) }; 
+    websocket.onmessage = function (evt) { onMessage(evt) }; 
+    websocket.onerror = function (evt) { onError(evt) }; 
+    function onOpen(evt) { 
+        console.log("Connected to WebSocket server."); 
+    } 
+    function onClose(evt) { 
+        console.log("Disconnected"); 
+    } 
+    function onMessage(evt) { 
+        console.log('Retrieved data from server: ' + evt.data); 
+    } 
+    function onError(evt) { 
+        console.log('Error occured: ' + evt); 
+    }
+    return;
 
     $("#action").click(function () {
         Client.opTest(client0);
--- server/server.py
+++ server/server.py
@@ -5,70 +5,66 @@ import random
 import aiomysql
 import sys
 
-conn_info = {}
+class Client:
 
-async def sendCoro(websocket):
-    try:
-        while True:
-            msg = await websocket.recv()
-            print("recv " + msg)
-            for q in list(conn_info.values()):
-                await q['queue'].put(msg)
-    except websockets.exceptions.ConnectionClosed as e:
-        print("sendCoro close")
-    finally:
-        conn_info[websocket]['rc'].cancel()
+    allClient = set()
 
-async def recvCoro(websocket):
-    try:
-        while True:
-            msg = await conn_info[websocket]['queue'].get()
-            print("send " + msg)
-            await websocket.send(msg)
-    except websockets.exceptions.ConnectionClosed as e:
-        print("recvCoro close")
-    finally:
-        conn_info[websocket]['sc'].cancel()
+    @asyncio.coroutine
+    def sendMsgAll(msg):
+        for client in Client.allClient:
+            yield from client.sendMsg(msg)
 
-async def serve(websocket, path):
-    rc = asyncio.ensure_future(recvCoro(websocket))
-    sc = asyncio.ensure_future(sendCoro(websocket))
-    conn_info[websocket] = {'queue':asyncio.Queue(),
-        'rc': rc, 'sc': sc}
-    await asyncio.wait([sc, rc])
-    conn_info.pop(websocket)
-    print("close conn");
+    def __init__(self, websocket):
+        self.websocket = websocket
+        self.sendQueue = asyncio.Queue()
+        Client.allClient.add(self)
 
-pool = None
+    @asyncio.coroutine
+    def loop(self):
+        self.rc = asyncio.ensure_future(self.send)
+        self.sc = asyncio.ensure_future(sendCoro(websocket))
+        yield from asyncio.wait([sc, rc])
+        Client.allClient.remove(websocket)
+        print("client loop end")
 
-async def create_db_pool():
-    global pool
-    pool = await aiomysql.create_pool(
-        host='127.0.0.1',
-        user='root',
-        password='',
-        db='ims')
+    @asyncio.coroutine
+    def sendMsg(self, msg):
+        yield from self.sendQueue.put(msg)
 
-async def test_db_pool():
-    async with pool.acquire() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute('select * from user')
+    @asyncio.coroutine
+    def sendCoro(self):
+        try:
             while True:
-                r = await cur.fetchone()
-                if not r: break
-                print(str(r))
+                msg = yield from self.sendQueue.get()
+                print("send " + msg)
+                yield from self.websocket.send(msg);
+        except websockets.exceptions.ConnectionClosed as e:
+            print("sendCoro close")
+        finally:
+            self.rc.cancel()
 
-async def close_db_pool():
-    pool.close()
-    await pool.wait_closed()
+    @asyncio.coroutine
+    def recvCoro(self):
+        try:
+            while True:
+                msg = yield from self.websocket.recv()
+                yield from onRecvMsg(msg)
+        except websockets.exceptions.ConnectionClosed as e:
+            print("recvCoro close")
+        finally:
+            self.sc.cancel()
+
+    @asyncio.coroutine
+    def onRecvMsg(self, msg):
+        yield from Client.sendMsgAll(msg)
+
+@asyncio.coroutine
+def serve(websocket, path):
+    print("new websocket" + path)
+    client = Client(websocket)
+    yield from client.loop()
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(create_db_pool())
-loop.run_until_complete(test_db_pool())
-loop.run_until_complete(close_db_pool())
-
-sys.exit(0);
-
 start_server = websockets.serve(serve, '127.0.0.1', 8000)
 loop.run_until_complete(start_server)
 
@@ -77,3 +73,29 @@ try:
 except KeyboardInterrupt as e:
     print("exit")
 
+'''
+pool = None
+async def create_db_pool():
+    global pool
+    pool = await aiomysql.create_pool(
+        host='127.0.0.1',
+        user='root',
+        password='',
+        db='ims')
+async def test_db_pool():
+    async with pool.acquire() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute('select * from user')
+            while True:
+                r = await cur.fetchone()
+                if not r: break
+                print(str(r))
+async def close_db_pool():
+    pool.close()
+    await pool.wait_closed()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(create_db_pool())
+loop.run_until_complete(test_db_pool())
+loop.run_until_complete(close_db_pool())
+sys.exit(0);
+'''
