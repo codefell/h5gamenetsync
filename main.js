85f5ca567c897ce17cbc8cebd943ce44162d6ccc
WangShuai <3087725379@qq.com>
2017-04-01 11:35:23 +0800

    base h5 demo framework

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 $(function () {
-    initScene("WebGLoutput0");
-    initScene("WebGLoutput1");
+    initScene("WebGLoutput");
     initEvent();
+    UpdateHandles.addHandle(function () {
+        if (global.event.keydown) {
+            console.log("key down", global.event.key);
+        }
+        if (global.event.mouseKeyDown) {
+            console.log("mouse down", global.event.mousePos);
+        }
+    });
 });
--- scene.js
+++ scene.js
@@ -1,21 +1,31 @@
+var global = {
+    event: {},
+};
 function initScene(eid) {
 
     // create a scene, that will hold all our elements such as objects, cameras and lights.
     var scene = new THREE.Scene();
+    global.scene = scene;
+
+    var je = $("#" + eid);
+    var width = je.width();
+    var height = je.height();
+    global.width = width;
+    global.height = height;
 
     // create a camera, which defines where we're looking at.
     //var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var camera = new THREE.OrthographicCamera(-100, 100, 100, -100, 1, 1000);
+    var camera = new THREE.OrthographicCamera(-width / 2, width / 2, height / 2, -height / 2, 1, 1000);
 
     // create a render and set the size
     var renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor(new THREE.Color(0xEEEEEE));
+    renderer.setClearColor(new THREE.Color(0x0));
     //renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setSize(200, 200);
+    renderer.setSize(width, height);
 
     // create the ground plane
     var planeGeometry = new THREE.PlaneGeometry(100, 100);
-    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
+    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xFF0000});
     var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
     // rotate and position the plane
@@ -48,14 +58,87 @@ function initScene(eid) {
 
 function initEvent() {
     $(document).keydown(function (e) {
-            console.log(String.fromCharCode(e.which));
-            });
-    $("#WebGLoutput0").click(function (e) {
-            var rect0 = $("#WebGLoutput0")[0].getBoundingClientRect();
-            var x = Math.floor(e.clientX - rect0.left);
-            var y = Math.floor(e.clientY - rect0.top);
-            x = -(100 - x);
-            y = 100 - y;
-            console.log(x, y);
-            });
+        var key = String.fromCharCode(e.which);
+        global.event.keydown = true;
+        global.event.key = key;
+    });
+    $("#WebGLoutput").click(function (e) {
+        var rect = $("#WebGLoutput")[0].getBoundingClientRect();
+        var x = Math.floor(e.clientX - rect.left);
+        var y = Math.floor(e.clientY - rect.top);
+        x = -(global.width / 2- x);
+        y = global.height / 2- y;
+        global.event.mouseKeyDown = true;
+        global.event.mousePos = {x: x, y: y};
+    });
 }
+
+function time() {
+    return (new Date()).getTime() / 1000;
+}
+
+var UpdateHandles = {
+    startTime: 0,
+    lastUpdateTime: 0,
+    time: 0,
+    deltaTime: 0,
+    handles: [],
+    init: function() {
+        UpdateHandles.startTime = time();
+        UpdateHandles.lastUpdateTime = 
+            UpdateHandles.startTime;
+        UpdateHandles.time = UpdateHandles.startTime;
+        UpdateHandles.deltaTime = 0;
+    },
+    addHandle: function (handle) {
+        for (var i = 0; i < this.handles.length; i++) {
+            if (this.handles[i] == null) {
+                this.handles[i] = handle;
+                return;
+            }
+        }
+        this.handles.push(handle);
+    },
+    delHandle: function (handle) {
+        for (var i = 0; i < this.handles.length; i++) {
+            if (this.handles[i] == handle) {
+                this.handles[i] = null;
+                return;
+            }
+        }
+    },
+    update: function () {
+        UpdateHandles.time = time();
+        UpdateHandles.deltaTime = 
+            UpdateHandles.time - UpdateHandles.lastUpdateTime;
+        
+        for (i in UpdateHandles.handles) {
+            handle = UpdateHandles.handles[i];
+            if (handle) {
+                handle();
+            }
+        }
+        global.event = {};
+        UpdateHandles.lastUpdateTime = UpdateHandles.time;
+        UpdateHandles.deltaTime = 0; 
+    },
+    addMethodUpdate: function (o) {
+        var f = function () {
+            o.update();
+        };
+        UpdateHandles.addHandle(f);
+        return f;
+    },
+    addUpdate: function (fn, o) {
+        var f = function () {
+            fn(o);
+        };
+        UpdateHandles.addHandle(f);
+        return f;
+    },
+};
+
+$(function () {
+    UpdateHandles.init();
+    setInterval(UpdateHandles.update, 1000/60);
+});
